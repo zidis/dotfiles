134c3b517555e46fbda86b83f9bff9dcb0b9e831
zidis <giannesalha@gmail.com>
2021-02-09 18:36:15 +0200

    change symlink.sh for install.sh

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# packages
+echo Installing packages...
+
+# redshift
+pacman -S redshift
+
+# tty-clock
+yay -S tty-clock
+
+# symlinks
+echo Creating symlinks...
+
+# qtile
+ln -s /home/zidis/dotfiles/qtile/config.py /home/zidis/.config/qtile
+
+# bash
+ln -s /home/zidis/dotfiles/bash/.bashrc /home/zidis
+
+# alacritty
+mkdir -p /home/zidis/.config/test
+ln -s /home/zidis/dotfiles/alacritty/alacritty.yml /home/zidis/.config/alacritty
+
+# vim
+ln -s /home/zidis/dotfiles/vim/.vimrc /home/zidis
+
+# neofetch
+ln -s /home/zidis/dotfiles/neofetch/config.conf /home/zidis/.config/neofetch
+
+echo Done!
