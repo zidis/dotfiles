6187fa0d106dcffb70ab7ce31cfe50c0fccc00cf
Giannis Alchazidis <31704637+zidis@users.noreply.github.com>
2021-02-10 01:31:58 +0200

    delete symlink.sh

--- symlink.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ln -s /home/zidis/dotfiles/qtile/config.py /home/zidis/.config/qtile
-ln -s /home/zidis/dotfiles/alacritty/alacritty.yml /home/zidis/.config/alacritty
-ln -s /home/zidis/dotfiles/bash/.bashrc /home/zidis
-ln -s /home/zidis/dotfiles/neofetch/config.conf /home/zidis/.config/neofetch
-ln -s /home/zidis/dotfiles/vim/.vimrc /home/zidis
-echo Finished!
